a1a1b7a1657b883fb876ef2d8f541aded16f365c
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-02 18:49:51 -0300

    Remove Hello, World!
    Changes to be committed:
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,4 @@
-use axum::{extract::Path, http::StatusCode, response::IntoResponse, routing::get, Router};
-
-async fn greet(Path(name): Path<String>) -> impl IntoResponse {
-    format!("Hello, {name}!")
-}
-/// Teste
-async fn root() -> &'static str {
-    "Hello, World!"
-}
+use axum::{http::StatusCode, routing::get, Router};
 
 /// Return `200 OK` if the API is running and is accessible
 async fn health_check() -> StatusCode {
@@ -15,10 +7,7 @@ async fn health_check() -> StatusCode {
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/", get(root))
-        .route("/:name", get(greet))
-        .route("/health_check", get(health_check));
+    let app = Router::new().route("/health_check", get(health_check));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
