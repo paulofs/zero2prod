a4f2225782db8bd21b5cb562206f67d0da90a680
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-02 18:23:22 -0300

    Add check_health API
    Changes to be committed:
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::{extract::Path, response::IntoResponse, routing::get, Router};
+use axum::{extract::Path, http::StatusCode, response::IntoResponse, routing::get, Router};
 
 async fn greet(Path(name): Path<String>) -> impl IntoResponse {
     format!("Hello, {name}!")
@@ -8,11 +8,17 @@ async fn root() -> &'static str {
     "Hello, World!"
 }
 
+/// Return `200 OK` if the API is running and is accessible
+async fn health_check() -> StatusCode {
+    StatusCode::OK
+}
+
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(root))
-        .route("/:name", get(greet));
+        .route("/:name", get(greet))
+        .route("/health_check", get(health_check));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
