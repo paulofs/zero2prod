9cbf545b14dbaa449d27422951c8d9ae2da5c008
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-02 17:54:03 -0300

    feat: Add /:name handler
    Changes to be committed:
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{extract::Path, response::IntoResponse, routing::get, Router};
+
+async fn greet(Path(name): Path<String>) -> impl IntoResponse {
+    format!("Hello, {name}!")
+}
+/// Teste
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/:name", get(greet));
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
