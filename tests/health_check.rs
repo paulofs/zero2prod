143252bff67182e15a7cd3db3d499318ba5c32ef
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-03 18:28:15 -0300

    Create a new database everytime we run tests
    Changes to be committed:
    	new file:   migrations/20230903084321_create_subscriptions_table.sql
    	modified:   src/configuration.rs
    	modified:   tests/health_check.rs

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,11 @@
-use sqlx::{Connection, PgConnection, PgPool};
+use sqlx::Connection;
+use sqlx::Executor;
+use sqlx::PgConnection;
+use sqlx::PgPool;
 use std::net::TcpListener;
+use uuid::Uuid;
 use zero2prod::configuration::get_configuration;
+use zero2prod::configuration::DatabaseSettings;
 
 /// Spin up as an instance of the application
 /// and returns it's address (i.e. http://localhost:XXXX)
@@ -9,16 +14,37 @@ pub struct TestApp {
     pub db_pool: PgPool,
 }
 
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    // Create database
+    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+        .await
+        .expect("Failed to connect to Postgres");
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database.");
+
+    // Migrate database
+    let connection_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+    connection_pool
+}
+
 async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("0.0.0.0:0").expect("Failed to bind random port");
 
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
 
-    let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+
+    let connection_pool = configure_database(&configuration.database).await;
 
     let server =
         zero2prod::startup::run(listener, connection_pool.clone()).expect("Failed to bind address");
