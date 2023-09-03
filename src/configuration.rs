1f4e91a723849d528afc24ff4cd23ed9efe975b9
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-03 16:01:58 -0300

    Database configuration
    Changes to be committed:
    	new file:   .env
    	modified:   configuration.yaml
    	modified:   src/configuration.rs
    	modified:   tests/health_check.rs

--- src/configuration.rs
+++ src/configuration.rs
@@ -4,9 +4,10 @@ use config::{Config, File, FileFormat};
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16
+    pub application_port: u16,
 }
 
+// Database Settings
 #[derive(serde::Deserialize)]
 pub struct DatabaseSettings {
     pub username: String,
@@ -14,6 +15,15 @@ pub struct DatabaseSettings {
     pub port: u16,
     pub host: String,
     pub database_name: String,
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,6 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 
 /// Spin up as an instance of the application
 /// and returns it's address (i.e. http://localhost:XXXX)
@@ -37,8 +39,16 @@ async fn health_check_works() {
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let client = reqwest::Client::new();
 
+    // Act
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let respose = client
         .post(&format!("{}/subscriptions", &app_address))
@@ -50,6 +60,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     //Assert
     assert_eq!(200, respose.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
