dd2d610a8e8dab4f150d9ac0e5ab1a3ee64e2a80
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-03 14:03:19 -0300

    Add postgres_docker scripts
    Changes to be committed:
    	new file:   scripts/init_db_postgres.sh
    	new file:   scripts/init_db_postgres_docker.sh
    	new file:   scripts/init_db_sqlite3.sh
    	new file:   sql_scripts/create_subscriptions_table.sql

---
+++ sql_scripts/create_subscriptions_table.sql
@@ -0,0 +1,9 @@
+-- Create Subscriptions Table
+CREATE TABLE subscriptions(
+    id uuid NOT NULL,
+    PRIMARY KEY (id),
+    email TEXT NOT NULL UNIQUE,
+    name TEXT NOT NULL,
+    -- WARN: `timestampz` it's a postress type, need to change to something that works with sqlite3
+    subscribed_at timestampz NOT NULL
+);
