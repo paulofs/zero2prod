cda4f0fc14598c3a5ce695b6a21d2825e407432a
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-03 16:29:43 -0300

    add connection pool
    Changes to be committed:
    	modified:   src/main.rs
    	modified:   src/startup.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 //! src/main.rs
-use std::net::TcpListener;
+use std::{net::TcpListener, time::Duration};
+use sqlx::postgres::PgPoolOptions;
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> hyper::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgPoolOptions::new()
+        .max_connections(5)
+        .acquire_timeout(Duration::from_secs(3))
+        .connect(&configuration.database.connection_string())
+        .await.expect("Failed to connect");
 
     let address = format!("0.0.0.0:{}", configuration.application_port);
 
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
-    run(listener)?.await
+    run(listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -5,16 +5,19 @@ use std::net::TcpListener;
 use crate::routes::{health_check, subscribe};
 use axum::{
     routing::{get, post, IntoMakeService},
-    Router,
+    Router, Extension,
 };
 use hyper::server::conn::AddrIncoming;
+use sqlx::{Pool, Postgres};
 // WARN: That's ugly
 pub fn run(
     listener: TcpListener,
+    connection: Pool<Postgres>
 ) -> hyper::Result<hyper::Server<AddrIncoming, IntoMakeService<Router>>> {
     let app = Router::new()
         .route("/health_check", get(health_check))
-        .route("/subscriptions", post(subscribe));
+        .route("/subscriptions", post(subscribe))
+        .layer(Extension(connection));
 
     let server = axum::Server::from_tcp(listener)?.serve(app.into_make_service());
     Ok(server)
