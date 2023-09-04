90ebb667b37c8ec4f9bcfce57add43bd94d2c4f0
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-04 16:58:15 -0300

    refactor
    Changes to be committed:
    	modified:   scripts/init_db.sh

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -44,8 +44,7 @@ if [[ -z "${SKIP_DOCKER}" ]]; then
 fi
 
 # Keep pinging Postgres until it`s ready to accept commands
-export PGPASSWORD="{DB_PASSWORD}"
-until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+until PGPASSWORD="${DB_PASSWORD}" psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
     >&2 echo "Postgres is still unavailable - sleeping"
     sleep 1
 done
