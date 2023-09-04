60a7887bd634975f8adaab80f01364b790a40282
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-03 22:12:42 -0300

    Move telemetry/logging related code to telemetry.rs
    Changes to be committed:
    	modified:   src/lib.rs
    	modified:   src/main.rs
    	new file:   src/telemetry.rs

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,16 @@
 //! src/main.rs
 use sqlx::postgres::PgPoolOptions;
 use std::{net::TcpListener, time::Duration};
-use zero2prod::{configuration::get_configuration, startup::run};
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
-use tracing_log::LogTracer;
+use zero2prod::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
 
 #[tokio::main]
 async fn main() -> hyper::Result<()> {
-    // Setup tracer
-    LogTracer::init().expect("Failed to set logger");
-    let env_filter = EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new(
-        "zero2prod".into(),
-        std::io::stdout
-        );
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection = PgPoolOptions::new()
