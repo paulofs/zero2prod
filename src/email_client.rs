c26641fb3cb80f94d5e761726eae8a7d0af9b0ff
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-05 16:22:00 -0300

    Add email_client
    Changes to be committed:
    	new file:   src/email_client.rs
    	modified:   src/lib.rs

---
+++ src/email_client.rs
@@ -0,0 +1,19 @@
+//! src/email_client.rs
+
+use crate::domain::SubscriberEmail;
+
+pub struct EmailClient {
+    sender: SubscriberEmail,
+}
+
+impl EmailClient {
+    pub async fn send_email(
+        &self,
+        recipient: SubscriberEmail,
+        subject: &str,
+        html_content: &str,
+        text_content: &str,
+    ) -> Result<(), String> {
+        todo!()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //! src/lib.rs
 pub mod configuration;
 pub mod domain;
+pub mod email_client;
 pub mod routes;
 pub mod startup;
 pub mod telemetry;
