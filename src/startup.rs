21e1b5938a9458a3c0f8d743d72ace4be6d67097
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-05 16:39:19 -0300

    Refactor
    Changes to be committed:
    	modified:   src/startup.rs

--- src/startup.rs
+++ src/startup.rs
@@ -22,24 +22,24 @@ pub fn run(
     listener: TcpListener,
     db_pool: Pool<Postgres>,
 ) -> hyper::Result<hyper::Server<AddrIncoming, IntoMakeService<Router>>> {
+    let tracer_middleware = ServiceBuilder::new()
+        .set_x_request_id(MakeRequestUuid)
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(
+                    DefaultMakeSpan::new()
+                        .include_headers(true)
+                        .level(Level::INFO),
+                )
+                .on_response(DefaultOnResponse::new().include_headers(true)),
+        )
+        .propagate_x_request_id();
+
     let app = Router::new()
         .route("/health_check", get(health_check))
         .route("/subscriptions", post(subscribe))
         .layer(Extension(db_pool))
-        .layer(
-            ServiceBuilder::new()
-                .set_x_request_id(MakeRequestUuid)
-                .layer(
-                    TraceLayer::new_for_http()
-                        .make_span_with(
-                            DefaultMakeSpan::new()
-                                .include_headers(true)
-                                .level(Level::INFO),
-                        )
-                        .on_response(DefaultOnResponse::new().include_headers(true)),
-                )
-                .propagate_x_request_id(),
-        );
+        .layer(tracer_middleware);
     let server = axum::Server::from_tcp(listener)?.serve(app.into_make_service());
     Ok(server)
 }
