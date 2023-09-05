51ae62834a7f3c5b079e7dcb88f54b3190fb3420
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-05 18:33:31 -0300

    Refactor EmailClient for lower memory footprint
    Changes to be committed:
    	modified:   src/email_client.rs

--- src/email_client.rs
+++ src/email_client.rs
@@ -35,11 +35,11 @@ impl EmailClient {
     ) -> Result<(), reqwest::Error> {
         let url = self.base_url.join("/email").expect("Failed to join url");
         let request_body = SendEmailRequest {
-            from: self.sender.as_ref().to_owned(),
-            to: recipient.as_ref().to_owned(),
-            subject: subject.to_owned(),
-            html_body: html_content.to_owned(),
-            text_body: text_content.to_owned(),
+            from: self.sender.as_ref(),
+            to: recipient.as_ref(),
+            subject,
+            html_body: html_content,
+            text_body: text_content,
         };
         self.http_client
             .post(url)
@@ -56,12 +56,12 @@ impl EmailClient {
 
 #[derive(serde::Serialize)]
 #[serde(rename_all = "PascalCase")]
-struct SendEmailRequest {
-    from: String,
-    to: String,
-    subject: String,
-    html_body: String,
-    text_body: String,
+struct SendEmailRequest<'a> {
+    from: &'a str,
+    to: &'a str,
+    subject: &'a str,
+    html_body: &'a str,
+    text_body: &'a str,
 }
 
 #[cfg(test)]
