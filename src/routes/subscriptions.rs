3f787ee03fe5870dab095a9db92ffefa460b0632
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-06 20:55:37 -0300

    Refactor
    Changes to be committed:
    	modified:   src/routes/subscriptions.rs

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -35,20 +35,7 @@ pub async fn subscribe(
     if insert_subscriber(&db_pool, &new_subscriber).await.is_err() {
         return StatusCode::INTERNAL_SERVER_ERROR;
     };
-    let confirmation_link = "https://my-api.com/subscriptions/confirm";
-    if email_client
-        .send_email(
-            new_subscriber.email,
-            "Welcome!",
-            &format!(
-                "Welcome to our newsletter!<br />\
-                Click <a href=\"{}\">here</a> to confirm your subscription.",
-                confirmation_link
-            ), &format!(
-                "Welcome to our newsletter!\nVisit {} to confirm your subscription.",
-                confirmation_link
-            ),
-        )
+    if send_confirmation_email(&email_client, new_subscriber)
         .await
         .is_err()
     {
@@ -65,6 +52,24 @@ impl TryFrom<FormData> for NewSubscriber {
         let email = SubscriberEmail::parse(value.email)?;
         Ok(Self { email, name })
     }
+}
+pub async fn send_confirmation_email(
+    email_client: &EmailClient,
+    new_subscriber: NewSubscriber,
+) -> Result<(), reqwest::Error> {
+    let confirmation_link = "https://my-api.com/subscriptions/confirm";
+    let plain_body = format!(
+        "Welcome to our newsletter!<br />\
+                Click <a href=\"{}\">here</a> to confirm your subscription.",
+        confirmation_link
+    );
+    let html_body = format!(
+        "Welcome to our newsletter!\nVisit {} to confirm your subscription.",
+        confirmation_link
+    );
+    email_client
+        .send_email(new_subscriber.email, "Welcome!", &html_body, &plain_body)
+        .await
 }
 
 #[tracing::instrument(
