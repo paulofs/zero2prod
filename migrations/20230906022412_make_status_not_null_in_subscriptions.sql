1620584c1aa297b31d6402f70db34d239424a2a9
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-05 23:27:39 -0300

    Make status obrigatory
    Changes to be committed:
    	deleted:    .sqlx/query-793f0df728d217c204123f12e4eafd6439db2d49d0cb506618ae9e780c7e0558.json
    	new file:   migrations/20230906022412_make_status_not_null_in_subscriptions.sql

---
+++ migrations/20230906022412_make_status_not_null_in_subscriptions.sql
@@ -0,0 +1,9 @@
+-- Add migration script here
+BEGIN;
+    -- Backfill `status` for historical entries
+    UPDATE subscriptions
+        SET status = 'confirmed'
+        WHERE status IS NULL;
+    -- Make `status` mandatory
+    ALTER TABLE subscriptions ALTER COLUMN status SET NOT NULL;
+COMMIT;
