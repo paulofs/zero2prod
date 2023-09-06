c125b00cf5463fa1ef1769e07d60713e659879fd
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-06 19:05:35 -0300

    Mail tests
    Changes to be committed:
    	modified:   Cargo.lock
    	modified:   Cargo.toml
    	modified:   tests/api/helpers.rs
    	modified:   tests/api/subscriptions.rs

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -1,4 +1,9 @@
 //! tests/api/subscriptions.rs
+use wiremock::{
+    matchers::{method, path},
+    Mock, ResponseTemplate,
+};
+
 use crate::helpers::spawn_app;
 
 #[tokio::test]
@@ -71,4 +76,24 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
             description
         );
     }
+}
+
+#[tokio::test]
+async fn subscribe_sends_a_confirmation_email_for_valid_data() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    // Act
+    app.post_subscriptions(body.into()).await;
+
+    // Assert
+    // Mock asserts on drop
 }
