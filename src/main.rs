38745cfd40b67827113901075889ce0aec6675e1
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-05 21:43:25 -0300

    Refactor
    Changes to be committed:
    	modified:   Cargo.lock
    	modified:   Cargo.toml
    	modified:   src/main.rs
    	modified:   src/startup.rs

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,19 @@
 //! src/main.rs
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
 use zero2prod::{
     configuration::get_configuration,
-    email_client::EmailClient,
-    startup::run,
+    startup::build,
     telemetry::{get_subscriber, init_subscriber},
 };
 
 #[tokio::main]
-async fn main() -> hyper::Result<()> {
+async fn main() -> anyhow::Result<()> {
+    // Start logging
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
-    let configuration = get_configuration().expect("Failed to read configuration.");
-    let db_pool = PgPoolOptions::new()
-        .max_connections(5)
-        .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(configuration.database.with_db());
-
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    let listener = TcpListener::bind(address).expect("Failed to bind random port");
-    run(listener, db_pool, email_client)?.await
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let server = build(configuration).await?;
+    server.await?;
+    // TODO: Transform this erron into sdt::io
+    Ok(())
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,8 +1,10 @@
 //! src/startup.rs
 // see: https://github.com/tokio-rs/axum/blob/main/examples/sqlx-postgres/src/main.rs
+
 use std::net::TcpListener;
 
 use crate::{
+    configuration::Settings,
     email_client::EmailClient,
     routes::{health_check, subscribe},
 };
@@ -11,8 +13,7 @@ use axum::{
     Extension, Router,
 };
 use hyper::server::conn::AddrIncoming;
-use sqlx::{Pool, Postgres};
-
+use sqlx::PgPool;
 use tower::ServiceBuilder;
 use tower_http::{
     request_id::MakeRequestUuid,
@@ -21,12 +22,43 @@ use tower_http::{
 };
 use tracing::Level;
 // WARN: That's ugly
+
+pub async fn build(
+    configuration: Settings,
+) -> anyhow::Result<RunningServer> {
+    let connection_pool = sqlx::postgres::PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.database.with_db());
+
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+    let timeout = configuration.email_client.timeout();
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+        timeout,
+    );
+
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    let listener = std::net::TcpListener::bind(address)?;
+
+    run(listener, connection_pool, email_client)
+}
+
+pub type RunningServer = hyper::Server<AddrIncoming, IntoMakeService<Router>>;
+
 pub fn run(
     listener: TcpListener,
-    db_pool: Pool<Postgres>,
+    db_pool: PgPool,
     email_client: EmailClient,
-) -> hyper::Result<hyper::Server<AddrIncoming, IntoMakeService<Router>>> {
-    let tracer_middleware = ServiceBuilder::new()
+) -> anyhow::Result<RunningServer> {
+    let middleware = ServiceBuilder::new()
         .set_x_request_id(MakeRequestUuid)
         .layer(
             TraceLayer::new_for_http()
@@ -44,7 +76,8 @@ pub fn run(
         .route("/subscriptions", post(subscribe))
         .layer(Extension(db_pool))
         .layer(Extension(email_client))
-        .layer(tracer_middleware);
+        .layer(middleware);
+
     let server = axum::Server::from_tcp(listener)?.serve(app.into_make_service());
     Ok(server)
 }
