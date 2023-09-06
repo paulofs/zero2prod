e89853bf296c1e0bd698bb003329b75bee1ab96c
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-06 20:47:57 -0300

    Test the existence of confirmation links on email
    Changes to be committed:
    	modified:   Cargo.lock
    	modified:   Cargo.toml
    	modified:   src/routes/subscriptions.rs
    	modified:   tests/api/subscriptions.rs

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -35,12 +35,19 @@ pub async fn subscribe(
     if insert_subscriber(&db_pool, &new_subscriber).await.is_err() {
         return StatusCode::INTERNAL_SERVER_ERROR;
     };
+    let confirmation_link = "https://my-api.com/subscriptions/confirm";
     if email_client
         .send_email(
             new_subscriber.email,
             "Welcome!",
-            "Welcome to our newsletter!",
-            "Welcome to our newsletter!",
+            &format!(
+                "Welcome to our newsletter!<br />\
+                Click <a href=\"{}\">here</a> to confirm your subscription.",
+                confirmation_link
+            ), &format!(
+                "Welcome to our newsletter!\nVisit {} to confirm your subscription.",
+                confirmation_link
+            ),
         )
         .await
         .is_err()
--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -1,4 +1,5 @@
 //! tests/api/subscriptions.rs
+use serde_json::from_slice;
 use wiremock::{
     matchers::{method, path},
     Mock, ResponseTemplate,
@@ -101,4 +102,40 @@ async fn subscribe_sends_a_confirmation_email_for_valid_data() {
 
     // Assert
     // Mock asserts on drop
+}
+
+#[tokio::test]
+async fn subscribe_sends_a_confirmation_email_with_a_link() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    // Act
+    app.post_subscriptions(body.into()).await;
+
+    // Assert
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+
+    let body: serde_json::Value = from_slice(&email_request.body).unwrap();
+
+    let get_link = |s: &str| {
+        let links: Vec<_> = linkify::LinkFinder::new()
+            .links(s)
+            .filter(|l| *l.kind() == linkify::LinkKind::Url)
+            .collect();
+            assert_eq!(links.len(), 1);
+        links[0].as_str().to_owned()
+    };
+
+    let html_link = get_link(&body["HtmlBody"].as_str().unwrap());
+    let text_link = get_link(&body["TextBody"].as_str().unwrap());
+
+    // The two links should be identical
+    assert_eq!(html_link, text_link);
 }
