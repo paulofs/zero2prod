74ef4a96548c4cf18098e156c8d0e8b8aa638c94
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-07 19:40:20 -0300

    Add base64 credential extractor
    Changes to be committed:
    	modified:   Cargo.lock
    	modified:   Cargo.toml
    	modified:   src/routes/newsletters.rs
    	modified:   tests/api/helpers.rs
    	modified:   tests/api/newsletter.rs

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,11 +1,60 @@
 use anyhow::Context;
-use axum::{debug_handler, response::IntoResponse, Extension, Json};
-use hyper::StatusCode;
+use axum::{
+    debug_handler,
+    http::{HeaderMap, HeaderValue},
+    response::IntoResponse,
+    Extension, Json,
+};
+use base64::Engine;
+use hyper::{
+    header::{self},
+    StatusCode,
+};
+
+use secrecy::Secret;
 use sqlx::PgPool;
 
 use crate::{domain::SubscriberEmail, email_client::EmailClient};
 
 use super::error_chain_fmt;
+
+struct Credentials {
+    username: String,
+    password: Secret<String>,
+}
+
+fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Error> {
+    let header_value = headers
+        .get("Authorization")
+        .context("The 'Authorization' header was missing")?
+        .to_str()
+        .context("The 'authorization' header was not a valid UTF8 string.")?;
+
+    let base64encoded_segment = header_value
+        .strip_prefix("Basic ")
+        .context("The authorization scheme was not 'Basic'.")?;
+    let decoded_bytes = base64::engine::general_purpose::STANDARD
+        .decode(base64encoded_segment)
+        .context("Failed to base64-decode 'Basic' credentials.")?;
+    let decoded_credentials = String::from_utf8(decoded_bytes)
+        .context("The decoded credential string is not valid UTF8.")?;
+
+    // Split into two segments, using ':' as delimitator
+    let mut credentials = decoded_credentials.splitn(2, ':');
+    let username = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("A username must be provided in 'Basic' auth."))?
+        .to_string();
+    let password = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("A password must be provided in 'Basic' auth."))?
+        .to_string();
+
+    Ok(Credentials {
+        username,
+        password: Secret::new(password),
+    })
+}
 
 #[derive(serde::Deserialize)]
 pub struct BodyData {
@@ -21,6 +70,8 @@ pub struct Content {
 
 #[derive(thiserror::Error)]
 pub enum PublishError {
+    #[error("Authentication failed")]
+    AuthError(#[source] anyhow::Error),
     #[error(transparent)]
     UnexpectedError(#[from] anyhow::Error),
 }
@@ -35,6 +86,14 @@ impl IntoResponse for PublishError {
     fn into_response(self) -> axum::response::Response {
         match self {
             PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+            PublishError::AuthError(_) => {
+                let mut response = StatusCode::UNAUTHORIZED.into_response();
+                let header_value = HeaderValue::from_str(r#"Basic realm="publish""#).unwrap();
+                response
+                    .headers_mut()
+                    .insert(header::WWW_AUTHENTICATE, header_value);
+                response
+            }
         }
     }
 }
@@ -42,8 +101,10 @@ impl IntoResponse for PublishError {
 pub async fn publish_newsletter(
     Extension(db_pool): Extension<PgPool>,
     Extension(email_client): Extension<EmailClient>,
+    headers: HeaderMap,
     Json(body): Json<BodyData>,
 ) -> Result<StatusCode, PublishError> {
+    let _credentials = basic_authentication(&headers).map_err(PublishError::AuthError)?;
     let subscribers = get_confirmed_subscribers(&db_pool).await?;
     for subscriber in subscribers {
         match subscriber {
