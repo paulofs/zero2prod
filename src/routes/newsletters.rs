896c30689ed745ec862b15e7953ceeab36bef5f3
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-07 17:06:05 -0300

    Chapter 9.6
    Changes to be committed:
    	modified:   src/routes/newsletters.rs
    	modified:   tests/api/helpers.rs
    	modified:   tests/api/newsletter.rs

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,5 +1,67 @@
+use axum::{debug_handler, response::IntoResponse, Extension, Json};
 use hyper::StatusCode;
+use sqlx::PgPool;
 
-pub async fn publish_newsletter() -> StatusCode {
-    StatusCode::OK
+use super::error_chain_fmt;
+
+#[derive(serde::Deserialize)]
+pub struct BodyData {
+    title: String,
+    content: Content,
+}
+
+#[derive(serde::Deserialize)]
+pub struct Content {
+    html: String,
+    text: String,
+}
+
+#[derive(thiserror::Error)]
+pub enum PublishError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for PublishError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl IntoResponse for PublishError {
+    fn into_response(self) -> axum::response::Response {
+        match self {
+            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+        }
+    }
+}
+#[debug_handler]
+pub async fn publish_newsletter(
+    Extension(db_pool): Extension<PgPool>,
+    Json(_body): Json<BodyData>,
+) -> Result<StatusCode, PublishError> {
+    let _subscribers = get_confirmed_subscribers(&db_pool).await?;
+    Ok(StatusCode::OK)
+}
+
+struct ConfirmedSubscriber {
+    email: String,
+}
+
+#[tracing::instrument(name = "Get confirmed subscribers", skip(db_pool))]
+async fn get_confirmed_subscribers(
+    db_pool: &PgPool,
+) -> Result<Vec<ConfirmedSubscriber>, anyhow::Error> {
+    let rows = sqlx::query_as!(
+        ConfirmedSubscriber,
+        r#"
+        SELECT email
+        FROM subscriptions
+        WHERE status = 'confirmed'
+        "#,
+    )
+    .fetch_all(db_pool)
+    .await?;
+
+    Ok(rows)
 }
--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -123,4 +123,13 @@ impl TestApp {
             .await
             .expect("Failed to execute request.")
     }
+
+    pub async fn post_newsletters(&self, body: serde_json::Value) -> reqwest::Response {
+        reqwest::Client::new()
+            .post(&format!("{}/newsletters", &self.address))
+            .json(&body)
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
 }
