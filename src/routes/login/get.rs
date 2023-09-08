8c4caf69509c458adecee1a606f9589f7aeed298
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-08 17:40:27 -0300

    Remove unused dependencies
    Changes to be committed:
    	modified:   Cargo.lock
    	modified:   Cargo.toml
    	modified:   src/routes/login/get.rs
    	modified:   tests/api/main.rs

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,81 +1,11 @@
-use axum::{debug_handler, extract::Query, response::IntoResponse, Extension};
-use hmac::{Hmac, Mac};
-use secrecy::ExposeSecret;
+use axum::{debug_handler, response::IntoResponse};
 
-use crate::startup::HmacSecret;
 #[debug_handler]
-pub async fn login_form(
-    Extension(secret): Extension<HmacSecret>,
-    query: Option<Query<QueryParams>>,
-) -> impl IntoResponse {
-    let error_html = match query {
-        None => "".into(),
-        Some(query) => match query.0.verify(&secret) {
-            Ok(error) => {
-                format!("<p><i>{}</i></p>", htmlescape::encode_minimal(&error))
-            }
-            Err(e) => {
-                tracing::warn!(
-                error.message = %e,
-                error.cause_chain = ?e,
-                "Failed to verify query parameters using the HMAC tag"
-                );
-                "".into()
-            }
-        },
-    };
+pub async fn login_form() -> impl IntoResponse {
+    let error_html: String = todo!();
+
     (
-        axum::http::StatusCode::OK,
-        [(
-            axum::http::header::CONTENT_TYPE,
-            ("text/html; charset=UTF-8"),
-        )],
-        axum::response::Html(format!(
-            r#"<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8">
-    <title>Login</title>
-</head>
-<body>
-    {error_html}
-    <form action="/login" method="post">
-        <label>Username
-            <input
-                type="text"
-                placeholder="Enter Username"
-                name="username"
-> </label>
-        <label>Password
-            <input
-                type="password"
-                placeholder="Enter Password"
-                name="password"
-> </label>
-        <button type="submit">Login</button>
-    </form>
-</body>
-</html>"#,
-        )),
+        [(axum::http::header::CONTENT_TYPE, "text/html; charset=UTF-8")],
+        ("/* HTML */"),
     )
-}
-
-#[derive(serde::Deserialize, Clone)]
-pub struct QueryParams {
-    error: String,
-    tag: String,
-}
-
-impl QueryParams {
-    fn verify(self, secret: &HmacSecret) -> Result<String, anyhow::Error> {
-        let tag = hex::decode(self.tag)?;
-        let query_string = format!("error={}", urlencoding::Encoded::new(&self.error));
-
-        let mut mac =
-            Hmac::<sha2::Sha256>::new_from_slice(secret.0.expose_secret().as_bytes()).unwrap();
-        mac.update(query_string.as_bytes());
-        mac.verify_slice(&tag)?;
-
-        Ok(self.error)
-    }
 }
