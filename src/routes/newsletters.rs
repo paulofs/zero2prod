9f97c4c9f398c06d145ed315d578917b625f0ec5
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-07 22:30:51 -0300

    Do not block the async executor
    Changes to be committed:
    	modified:   src/routes/newsletters.rs

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -13,7 +13,6 @@ use hyper::{
 };
 
 use secrecy::{ExposeSecret, Secret};
-use sha3::Digest;
 use sqlx::PgPool;
 
 use crate::{domain::SubscriberEmail, email_client::EmailClient};
@@ -171,41 +170,63 @@ async fn get_confirmed_subscribers(
     Ok(confirmed_subscribers)
 }
 
-async fn validate_credentials(
-    credentials: Credentials,
+#[tracing::instrument(name = "Get stored credentials", skip(username, db_pool))]
+async fn get_stored_credentials(
+    username: &str,
     db_pool: &PgPool,
-) -> Result<uuid::Uuid, PublishError> {
+) -> Result<Option<(uuid::Uuid, Secret<String>)>, anyhow::Error> {
     let row: Option<_> = sqlx::query!(
         r#"
         SELECT user_id, password_hash
         FROM users
         WHERE username = $1
         "#,
-        credentials.username,
+        username,
     )
     .fetch_optional(db_pool)
     .await
-    .context("Failed to perform a query to retrieve stored credentials.")
-    .map_err(PublishError::UnexpectedError)?;
+    .context("Failed to perform a query to retrieve stored credentials.")?
+    .map(|row| (row.user_id, Secret::new(row.password_hash)));
 
-    let (expected_password_hash, user_id) = match row {
-        Some(row) => (row.password_hash, row.user_id),
-        None => {
-            return Err(PublishError::AuthError(anyhow::anyhow!("Unknown username")));
-        }
-    };
+    Ok(row)
+}
 
-    let expected_password_hash = PasswordHash::new(&expected_password_hash)
+#[tracing::instrument(
+    name = "Verify password hash",
+    skip(expected_password_hash, password_canditate)
+)]
+fn verify_password_hash(
+    expected_password_hash: Secret<String>,
+    password_canditate: Secret<String>,
+) -> Result<(), PublishError> {
+    let expected_password_hash = PasswordHash::new(expected_password_hash.expose_secret())
         .context("Failed to parse hash in PHC string format.")
         .map_err(PublishError::UnexpectedError)?;
 
     Argon2::default()
         .verify_password(
-            credentials.password.expose_secret().as_bytes(),
+            password_canditate.expose_secret().as_bytes(),
             &expected_password_hash,
         )
         .context("Invalid password")
-        .map_err(PublishError::AuthError)?;
+        .map_err(PublishError::AuthError)
+}
 
+#[tracing::instrument(name = "Validate credentials", skip(credentials, db_pool))]
+async fn validate_credentials(
+    credentials: Credentials,
+    db_pool: &PgPool,
+) -> Result<uuid::Uuid, PublishError> {
+    let (user_id, expected_password_hash) = get_stored_credentials(&credentials.username, &db_pool)
+        .await
+        .map_err(PublishError::UnexpectedError)?
+        .ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username.")))?;
+
+    tokio::task::spawn_blocking(move || {
+        verify_password_hash(expected_password_hash, credentials.password)
+    })
+    .await
+    .context("Failed to spawn blocking task.")
+    .map_err(PublishError::UnexpectedError)??;
     Ok(user_id)
 }
