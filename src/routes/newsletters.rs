ca85a2d587481dd6465670b2350382d844a24a3f
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-07 23:28:07 -0300

    Make the validation validation of existing and not existing users the same
    Changes to be committed:
    	modified:   src/routes/newsletters.rs
    	modified:   tests/api/helpers.rs

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -219,17 +219,35 @@ async fn validate_credentials(
     credentials: Credentials,
     db_pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
-    let (user_id, expected_password_hash) = get_stored_credentials(&credentials.username, db_pool)
-        .await
-        .map_err(PublishError::UnexpectedError)?
-        .ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username.")))?;
+    let mut user_id = None;
+    let mut expected_password_hash = Secret::new(
+        "$argon2id$v=19$m=15000,t=2,p=1$\
+        gZiV/M1gPc22ElAH/Jh1Hw$\
+        CWOrkoo7oJBQ/iyh7uJ0LO2aLEfrHwTWllSAxT0zRno"
+            .to_string(),
+    );
+    if let Some((stored_user_id, stored_password_hash)) =
+        get_stored_credentials(&credentials.username, db_pool)
+            .await
+            .map_err(PublishError::UnexpectedError)?
+    {
+        user_id = Some(stored_user_id);
+        expected_password_hash = stored_password_hash;
+    }
 
-    let current_span = tracing::Span::current();
+    // let current_span = tracing::Span::current();
     spawn_blocking_with_tracing(move || {
-        current_span.in_scope(|| verify_password_hash(expected_password_hash, credentials.password))
+        verify_password_hash(expected_password_hash, credentials.password)
     })
     .await
     .context("Failed to spawn blocking task.")
     .map_err(PublishError::UnexpectedError)??;
-    Ok(user_id)
+    
+    // This is only set to `Some` if we found credentials in the store
+    // so, even if the default password ends up matching (somehow)
+    // with the provided password,
+    // we never authenticate a non-existing user.
+    user_id.ok_or_else(||
+        PublishError::AuthError(anyhow::anyhow!("Unknow username."))
+                       )
 }
--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,6 +1,6 @@
 //! tests/api/helpers.rs
 
-use argon2::{password_hash::SaltString, Argon2, PasswordHasher};
+use argon2::{password_hash::SaltString, Algorithm, Argon2, Params, PasswordHasher, Version};
 use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
@@ -38,10 +38,14 @@ impl TestUser {
         let salt = SaltString::generate(&mut rand::thread_rng());
         // We don't care about the exact Argon2 parameters here
         // given that it's for testing purposes!
-        let password_hash = Argon2::default()
-            .hash_password(self.password.as_bytes(), &salt)
-            .unwrap()
-            .to_string();
+        let password_hash = Argon2::new(
+            Algorithm::Argon2id,
+            Version::V0x13,
+            Params::new(15000, 2, 1, None).unwrap(),
+        )
+        .hash_password(self.password.as_bytes(), &salt)
+        .unwrap()
+        .to_string();
         sqlx::query!(
             "INSERT INTO users (user_id, username, password_hash)
             VALUES ($1, $2, $3)",
