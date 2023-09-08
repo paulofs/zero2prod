f32393a0eb31ab0547eb3d68366cce8e2bfae45c
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-08 17:59:20 -0300

    Show error message based on cookie value
    Changes to be committed:
    	modified:   Cargo.lock
    	modified:   Cargo.toml
    	modified:   src/routes/login/get.rs
    	modified:   tests/api/login.rs

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,11 +1,48 @@
-use axum::{debug_handler, response::IntoResponse};
+use axum::{
+    debug_handler,
+    response::{Html, IntoResponse},
+};
+use axum_extra::extract::CookieJar;
 
 #[debug_handler]
-pub async fn login_form() -> impl IntoResponse {
-    let error_html: String = todo!();
+pub async fn login_form(cookiejar: CookieJar) -> impl IntoResponse {
+    let error_html = match cookiejar.get("_flash") {
+        None => "".into(),
+        Some(cookie) => {
+            format!("<p><i>{}</i></p>", cookie.value())
+        }
+    };
 
     (
         [(axum::http::header::CONTENT_TYPE, "text/html; charset=UTF-8")],
-        ("/* HTML */"),
+        Html(format!(
+            r#"<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Login</title>
+</head>
+<body>
+    {error_html}
+    <form action="/login" method="post">
+        <label>Username
+            <input
+                type="text"
+                placeholder="Enter Username"
+                name="username"
+            > 
+        </label>
+        <label>Password
+            <input
+                type="password"
+                placeholder="Enter Password"
+                name="password"
+            > 
+        </label>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>"#,
+        )),
     )
 }
--- tests/api/login.rs
+++ tests/api/login.rs
@@ -21,5 +21,5 @@ async fn an_error_flash_message_is_set_on_failure() {
 
     // Act - Part 2
     let html_page = app.get_login_html().await;
-    assert!(html_page.contains(r#"<p><i>Authentication failed<i></p>"#));
+    assert!(html_page.contains(r#"<p><i>Authentication failed</i></p>"#));
 }
