7cb5a14701113d40aa1b16c0cecc399a0256cd6d
Paulo Vítor Sarmento <vsarmento@proton.me>
2023-09-09 15:01:55 -0300

    Migrate
    Changes to be committed:
    	modified:   Cargo.toml
    	new file:   migrations/20230909171425_seed_user.sql
    	modified:   sql_scripts/create_subscriptions_table.sql
    	modified:   tests/api/admin_dashboard.rs

--- sql_scripts/create_subscriptions_table.sql
+++ sql_scripts/create_subscriptions_table.sql
@@ -5,5 +5,6 @@ CREATE TABLE subscriptions(
     email TEXT NOT NULL UNIQUE,
     name TEXT NOT NULL,
     -- WARN: `timestampz` it's a postress type, need to change to something that works with sqlite3
-    subscribed_at timestamptz NOT NULL
+    subscribed_at timestamptz NOT NULL,
+    status TEXT NULL
 );
--- tests/api/admin_dashboard.rs
+++ tests/api/admin_dashboard.rs
@@ -1,4 +1,4 @@
-use crate::helpers::{spawn_app, assert_is_redirect_to};
+use crate::helpers::{assert_is_redirect_to, spawn_app};
 
 #[tokio::test]
 async fn you_must_be_logged_in_to_access_the_admin_dashboard() {
